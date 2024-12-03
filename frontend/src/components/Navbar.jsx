ea07e45e17a9e892c61ead712d8b0e902785ec6a
fattahali37 <fattahali37@gmail.com>
2024-12-03 19:45:02 +0500

    Navbar issue resolved + links added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,15 +6,21 @@ const Navbar = () => {
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
       <ul className="hidden md:flex items-start gap-5 font-medium">
-        <NavLink>
-          <li>Home</li>
-          <hr />
-          <li>All Doctors</li>
-          <hr />
-          <li>About</li>
-          <hr />
-          <li>Contact</li>
-          <hr />
+        <NavLink to="/">
+          <li className="py-1">Home</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to='/doctors'>
+          <li className="py-1">All Doctors</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to='/about'>
+          <li className="py-1">About</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to='/contact'>
+          <li className="py-1">Contact</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
       <div>
