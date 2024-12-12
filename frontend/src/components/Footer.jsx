5f2d9d34a9e0a09498fe9e98b2ea33d3b5341057
fattahali37 <fattahali37@gmail.com>
2024-12-12 16:42:09 +0500

    homepage completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,22 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* Left section */}
         <div>
-          <img src={assets.logo} alt="" />
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
         </div>
         {/* center section */}
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About us</li>
             <li>Contact us</li>
@@ -22,15 +27,17 @@ const Footer = () => {
         </div>
         {/* Right section */}
         <div>
-          <p>GET IN TOUCH</p>
-          <li>+1-212-7890</li>
-          <li>prescripto@gmail.com</li>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+1-212-7890</li>
+            <li>prescripto@gmail.com</li>
+          </ul>
         </div>
       </div>
-        {/* Copyright text */}
+      {/* Copyright text */}
       <div>
         <hr />
-        <p>Copyright © 2024 FattahAli - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright © 2024 FattahAli - All Right Reserved.</p>
       </div>
     </div>
   );
